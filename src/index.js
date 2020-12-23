af032d54b5f2ca01c2c8a22063e73ee4e2644e72
Mayrinika <mashuliy_u@mail.ru>
2020-12-23 21:54:33 +0500

    fix bugs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ function onExtensionClick(tab) {
     const inputList = document.querySelectorAll('input[type="text"]');
     for(const input of inputList) {
         const isDisabled = input.hasAttribute('disabled');
-        if((input.offsetWidth === 0 || input.offsetHeight === 0) || isDisabled) continue;
+		const isReadonly = input.hasAttribute('readonly');
+        if((input.offsetWidth === 0 || input.offsetHeight === 0) || isDisabled || isReadonly) continue;
         new EmojiWidget(input);
     }
 }
